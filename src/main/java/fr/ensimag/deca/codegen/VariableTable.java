1abef4483e28ec8e6d46020cb56d334d419fed1b
Gwennan Eliezer <gwennan.eliezer@grenoble-inp.org>
2022-01-11 14:39:22 +0100

    Implemented Register Manager

--- src/main/java/fr/ensimag/deca/codegen/VariableTable.java
+++ src/main/java/fr/ensimag/deca/codegen/VariableTable.java
@@ -1,3 +1,5 @@
+package fr.ensimag.deca.codegen;
+
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -5,13 +7,16 @@ import java.util.HashMap;
 import fr.ensimag.deca.tools.SymbolTable.Symbol;
 import fr.ensimag.deca.tree.AbstractDeclVar;
 import fr.ensimag.deca.tree.ListDeclVar;
+import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.ima.pseudocode.instructions.*;
 import fr.ensimag.ima.pseudocode.*;
 
 public class VariableTable {
-    private HashMap<Symbol, Integer> table = new HashMap<>();
+    private Map<Symbol, Integer> table = new HashMap<>();
     private int nextKey = 1;
-    public VariableTable(ListDeclVar vars) {
+    private DecacCompiler compiler;
+    public VariableTable(DecacCompiler compiler, ListDeclVar vars) {
+        this.compiler = compiler;
         List<AbstractDeclVar> varsList = vars.getList();
         for (int i = 0; i < varsList.size(); i++) {
             AbstractDeclVar variable = varsList.get(i);
@@ -22,11 +27,11 @@ public class VariableTable {
     }
     public void load(Symbol s, GPRegister dst) {
         int i = get(s);
-        new LOAD(new RegisterOffset(i, Register.LB), dst);
+        compiler.addInstruction(new LOAD(new RegisterOffset(i, Register.LB), dst));
     }
     public void store(Symbol s, GPRegister register) {
         int i = get(s);
-        new STORE(register, new RegisterOffset(i, Register.LB));
+        compiler.addInstruction(new STORE(register, new RegisterOffset(i, Register.LB)));
     }
     public int get(Symbol s) {
         return table.get(s);
