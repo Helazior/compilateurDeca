259a4e4a247a88cd27052a01c66c57d39281f9cf
Clement Gindrier <clement.gindrier@grenoble-inp.org>
2022-01-13 17:25:21 +0100

    De nouveaux tests

--- src/test/java/fr/ensimag/deca/tree/Gencode_ifThen_GE.java
+++ src/test/java/fr/ensimag/deca/tree/Gencode_ifThen_GE.java
@@ -5,38 +5,37 @@
  */
 package fr.ensimag.deca.tree;
 
-import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.deca.context.FloatType;
-import fr.ensimag.deca.context.Type;
-import fr.ensimag.deca.tools.SymbolTable;
-import fr.ensimag.ima.pseudocode.instructions.ADD;
-import fr.ensimag.ima.pseudocode.instructions.MUL;
 import fr.ensimag.deca.CompilerOptions;
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.tools.SymbolTable;
 
 /**
  *
  * @author Ensimag
  * @date 01/01/2022
  */
-public class ManualTestPrintMULfloatGencode {
+public class Gencode_ifThen_GE {
 
     public static AbstractProgram initTest1() {
         SymbolTable st = new SymbolTable();
-        Type float_t = new FloatType(st.create("float"));
         ListInst linst = new ListInst();
-        ListExpr exp1 = new ListExpr();
-        linst.add(new Print(false,exp1));
-        //3.2*2.9*4 = 37.12
-        Multiply p1 = new Multiply(new FloatLiteral(3.2f), new FloatLiteral(2.9f));
-        Multiply p2 = new Multiply(p1, new FloatLiteral(4.0f));
-        p1.setType(float_t);
-        p2.setType(float_t);
-        exp1.add(p2);
+        ListInst linsttrue = new ListInst();
+        ListInst linstfalse = new ListInst();
+        ListExpr exprtrue = new ListExpr();
+        ListExpr exprfalse = new ListExpr();
+        exprtrue.add(new StringLiteral("VRAI !"));
+        exprfalse.add(new StringLiteral("FAUX !"));
+        linsttrue.add(new Print(false, exprtrue));
+        linstfalse.add(new Print(false, exprfalse));
+        GreaterOrEqual p1 = new GreaterOrEqual(new IntLiteral(50), new IntLiteral(50));     // true
+        IfThenElse ite = new IfThenElse(p1, linsttrue, linstfalse);
+        linst.add(ite);
         AbstractProgram source =
                 new Program(
                         new ListDeclClass(),
                         new Main(new ListDeclVar(),linst));
         return source;
+
     }
 
     public static String gencodeSource(AbstractProgram source) {
@@ -48,7 +47,7 @@ public class ManualTestPrintMULfloatGencode {
     public static void test1() {
         AbstractProgram source = initTest1();
         //System.out.println("---- From the following Abstract Syntax Tree ----");
-        // source.prettyPrint(System.out);
+        //source.prettyPrint(System.out);
         //System.out.println("---- We generate the following assembly code ----");
         String result = gencodeSource(source);
         System.out.println(result);
