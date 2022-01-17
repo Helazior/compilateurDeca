390fe258ddccb478fa25b37289519b160a888bdf
Gwennan Eliezer <gwennan.eliezer@grenoble-inp.org>
2022-01-17 09:40:14 +0100

    Jacoco compile

--- src/test/script/test-options.sh
+++ src/test/script/test-options.sh
@@ -1,7 +1,7 @@
 exit_value=0
 
 if decac -h > tmp.out; then
-	if ! [[ -s tmp.out ]]; then
+	if ! [ -s tmp.out ]; then
 		echo "Erreur: il n'y a pas d'affichage d'usage sur l'option -h"
 		exit_value=1
 	fi
@@ -11,7 +11,7 @@ else
 fi
 
 if decac > tmp.out; then
-	if ! [[ -s tmp.out ]]; then
+	if ! [ -s tmp.out ]; then
 		echo "Erreur: il n'y a pas d'affichage d'usage sans argument"
 		exit_value=1
 	fi
@@ -20,12 +20,15 @@ else
 	exit_value=1
 fi
 
-if decac -r 2 src/test/deca/codegen/valid/provided/ecrit0.deca > tmp.out; then
+if decac -r 2 src/test/deca/codegen/valid/provided/ecrit0.deca 2> tmp.out; then
 	echo "Erreur: decac n'échoue pas avec un nombre de registes invalide"
 fi
 
-if decac -r 18 src/test/deca/codegen/valid/provided/ecrit0.deca > tmp.out; then
+if decac -r 18 src/test/deca/codegen/valid/provided/ecrit0.deca 2> tmp.out; then
 	echo "Erreur: decac n'échoue pas avec un nombre de registes invalide"
 fi
+
+
+echo "Options testées avec succès"
 
 exit $exit_value
