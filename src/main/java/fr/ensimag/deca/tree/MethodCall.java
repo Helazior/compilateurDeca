01ac5424c96f6022a180d67e68158e6821427c30
Clement Gindrier <clement.gindrier@grenoble-inp.org>
2022-01-20 12:28:10 +0100

    appel de méthode commencé

--- src/main/java/fr/ensimag/deca/tree/MethodCall.java
+++ src/main/java/fr/ensimag/deca/tree/MethodCall.java
@@ -1,12 +1,17 @@
 package fr.ensimag.deca.tree;
 
+import fr.ensimag.deca.codegen.RegisterManager;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.GPRegister;
 import fr.ensimag.ima.pseudocode.ImmediateString;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.PUSH;
 import fr.ensimag.ima.pseudocode.instructions.WSTR;
 import java.io.PrintStream;
 import org.apache.commons.lang.Validate;
@@ -34,6 +39,19 @@ public class MethodCall extends AbstractExpr {
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
                 throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    protected void codeGenExpr(DecacCompiler compiler){
+        RegisterManager regMan = compiler.getRegMan();
+        for (AbstractExpr expr : parametres.getList()) {
+            expr.codeGenExpr(compiler);
+            GPRegister regParameter = regMan.pop();
+            compiler.addInstruction(new PUSH(regParameter));
+        }
+
+        // On saute au label de la méthode
+        compiler.addInstruction(new BRA(new Label("bodyMethod." + getClass().getName() + "." + nomDeMethode)));
     }
 
     @Override
