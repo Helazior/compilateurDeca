6a02ec3f998fa2c06653826d62d7cf47d395839f
petiteam <matthieu.petiteau@grenoble-inp.org>
2022-01-21 16:12:53 +0100

    order fix

--- src/main/java/fr/ensimag/deca/DecacCompiler.java
+++ src/main/java/fr/ensimag/deca/DecacCompiler.java
@@ -5,6 +5,7 @@ import fr.ensimag.deca.syntax.DecaParser;
 import fr.ensimag.deca.syntax.DecaImportParser;
 import fr.ensimag.deca.tools.DecacInternalError;
 import fr.ensimag.deca.tree.AbstractProgram;
+import fr.ensimag.deca.tree.DeclClass;
 import fr.ensimag.deca.tree.LocationException;
 import fr.ensimag.ima.pseudocode.*;
 
@@ -21,8 +22,11 @@ import org.apache.log4j.Logger;
 import fr.ensimag.deca.tools.SymbolTable;
 import fr.ensimag.deca.tools.SymbolTable.Symbol;
 import fr.ensimag.deca.tree.Location;
+
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 
@@ -268,6 +272,37 @@ public class DecacCompiler {
      */
     private RegisterManager regManager;
 
+
+    public IMAProgram remplaceProgram(IMAProgram newProgram) {
+        IMAProgram oldProgram = program;
+        program = newProgram;
+        return oldProgram;
+    }
+
+    /**
+     * oldProgram se place au début du programme
+     * @param oldProgram
+     */
+    public void concatenateBeginningProgram(IMAProgram oldProgram) {
+        oldProgram.append(program);
+        program = oldProgram;
+    }
+
+    /**
+     * oldProgram se place au début du programme
+     * @param oldProgram
+     */
+    public void concatenateEndProgram(IMAProgram oldProgram) {
+        program.append(oldProgram);
+    }
+
+//region TREE NODE MAP
+    private final Map<Symbol,DeclClass> classNodes = new HashMap<>();
+
+//endregion
+
+//region TYPE ENVIRONMENT
+
     /**
      * Environnement des types définis lors de l'étape B de la compilation
      */
@@ -318,31 +353,9 @@ public class DecacCompiler {
         Symbol type = typeTable.create(typeName);
         return getType(type).getType();
     }
+//endregion
 
-    public IMAProgram remplaceProgram(IMAProgram newProgram) {
-        IMAProgram oldProgram = program;
-        program = newProgram;
-        return oldProgram;
-    }
-
-    /**
-     * oldProgram se place au début du programme
-     * @param oldProgram
-     */
-    public void concatenateBeginningProgram(IMAProgram oldProgram) {
-        oldProgram.append(program);
-        program = oldProgram;
-    }
-
-    /**
-     * oldProgram se place au début du programme
-     * @param oldProgram
-     */
-    public void concatenateEndProgram(IMAProgram oldProgram) {
-        program.append(oldProgram);
-    }
-
-
+//region EXP ENVIRONMENT
     /**
     * correspond à un environement de symboles hors des class et méthodes
     * ne sert que pour le parser en principe, car ensuite les symboles sont tous placé
@@ -380,8 +393,7 @@ public class DecacCompiler {
         return expTable.create(expName);
     }
 
-
-
+//endregion
 
 
 
